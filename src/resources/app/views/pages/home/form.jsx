3f9b870debd242e12429ccf1e5515c91929852da
cuongtv <truongcuong90@gmail.com>
2019-06-21 19:13:16 +0700

    Add plupload

--- src/resources/app/views/pages/home/form.jsx
+++ src/resources/app/views/pages/home/form.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
+import plupload from 'plupload'
 
-const UploadForm = () => (
-  <div>
-    <div>
-      <label> Upload Template</label>
+const UploadForm = () => {
+  const uploader = new plupload.Uploader({
+    browse_button: 'browse',
+    url: 'localhost:3006'
+  })
+ 
+  uploader.init()
+
+  return (
+    <div id="container">
+      <div>
+        <label> Upload Template</label>
+        <br/>
+        <button id="browse">Browse Files...</button>
+      </div>
       <br/>
-      <button>Upload</button>
+      <div>
+        <label> Upload Images</label>
+        <input type='radio' name='zip' value='zip' />Zip
+        <input type='radio' name='images' value='images' />Images
+        <br/>
+        <button id="browse">Browse Files...</button>
+      </div>
     </div>
-    <br/>
-    <div>
-      <label> Upload Images</label>
-      <input type='radio' name='zip' value='zip' />Zip
-      <input type='radio' name='images' value='images' />Images
-      <br/>
-      <button>Upload</button>
-    </div>
-  </div>
-)
+  )
+}
 
 export default UploadForm
