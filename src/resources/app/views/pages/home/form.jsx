d73be507f3c354f027c8ddcca13b13c243d81cb1
cuongtv <truongcuong90@gmail.com>
2019-06-24 18:35:30 +0700

    Add Fillter upload file Show list file select

--- src/resources/app/views/pages/home/form.jsx
+++ src/resources/app/views/pages/home/form.jsx
@@ -1,31 +1,128 @@
 import React from 'react'
 import plupload from 'plupload'
 
-const UploadForm = () => {
-  const uploader = new plupload.Uploader({
-    browse_button: 'browse',
-    url: 'localhost:3006'
-  })
- 
-  uploader.init()
+class UploadForm extends React.Component {
 
-  return (
-    <div id="container">
-      <div>
-        <label> Upload Template</label>
+  constructor(props) {
+    super(props)
+    this.state = {
+      files: [],
+      template: [],
+      mimeType: 'zip',
+      mimeTypesFilter: {
+        title : 'Zip files',
+        extensions : 'zip'
+      }
+    }
+
+    this.changeMimeType = this.changeMimeType.bind(this)
+  }
+
+  uploadTemplate() {
+    return new plupload.Uploader({
+      browse_button: 'browseTemplate',
+      url: 'http://localhost:3009',
+      init: {
+        FilesAdded: (up, files) => {
+          this.setState({ template: files })
+        }
+      },
+      filters : {
+        mime_types: [
+          { title : 'Image files', extensions : 'jpg,gif,png' },
+        ]
+      },
+      multi_selection: false
+    })
+  }
+
+  uploadItems() {
+    return new plupload.Uploader({
+      browse_button: 'browseFiles',
+      url: 'http://localhost:3009',
+      init: {
+        FilesAdded: (up, files) => {
+          this.setState({ files })
+        }
+      },
+      filters : {
+        mime_types: [
+          {
+            title : 'Zip files',
+            extensions : 'zip'
+          }
+        ]
+      }
+    })
+  }
+
+  componentDidMount() {
+    this.uploadTemplate().init()
+    this.uploadItems().init()
+  }
+
+  changeMimeType(event) {
+    const mimeType = event.target.value
+    if (mimeType === 'zip') {
+      this.setState({ mimeType })
+    } else {
+      this.setState({ mimeType })
+    }
+  }
+
+  render() {
+    const templateUpload = this.state.template.map((file, index) => {
+      return (
+        <div key = { index } >
+          <p>
+            { file.name }
+          </p>
+        </div>
+      )
+    })
+    const filesUpload = this.state.files.map((file, index) => {
+      return (
+        <div key = { index } >
+          <p>
+            { file.name }
+          </p>
+        </div>
+      )
+    })
+    return (
+      <div id="container">
+        <div>
+          <label> Upload Template</label>
+          <br/>
+          <button id="browseTemplate">Browse Template...</button>
+        </div>
+        <div>
+          { templateUpload }
+        </div>
         <br/>
-        <button id="browse">Browse Files...</button>
+        <div>
+          <label> Upload Images</label>
+          <input
+            type='radio'
+            name='zip'
+            value='zip'
+            onChange={ this.changeMimeType }
+            checked={ this.state.mimeType === 'zip' ? true : false  }/>Zip
+          <input
+            type='radio'
+            name='images'
+            value='images'
+            onChange={ this.changeMimeType }
+            checked={ this.state.mimeType === 'images' ? true : false  }/>Images
+          <br/>
+          <button id="browseFiles">Browse Files...</button>
+        </div>
+        <div>
+          { filesUpload }
+        </div>
       </div>
-      <br/>
-      <div>
-        <label> Upload Images</label>
-        <input type='radio' name='zip' value='zip' />Zip
-        <input type='radio' name='images' value='images' />Images
-        <br/>
-        <button id="browse">Browse Files...</button>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default UploadForm
