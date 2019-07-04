f2e1d88f935ea01013e0226042638d2b9497afc1
cuongtv <truongcuong90@gmail.com>
2019-07-04 16:12:24 +0700

    Update Ui config

--- src/resources/app/views/pages/home/template-config.jsx
+++ src/resources/app/views/pages/home/template-config.jsx
@@ -3,56 +3,85 @@ import styled, { css } from 'styled-components'
 
 
 const WrapperItem = styled.div`
-  border: solid 1px blue;
-  grid-gap: 16px;
+  border: solid 1px #007FFF;
+  grid-gap: 8px;
   display: grid;
-  width: 420px;
+  width: 400px;
   grid-template-rows: repeat(3, 128px);
   grid-template-columns: repeat(3,128px);
   margin: auto;
   text-align: center;
   padding: 8px;
 `
-const Item = styled.div`
-  border: solid 1px red;
+const Item = styled.button`
+
+  ${
+    ({ active, theme }) => active ?
+    css`
+      background: ${ theme.secondary.base };
+      color: ${ theme.secondary.on.base };
+    ` :
+    css`
+      background: 'none';
+    `
+  }
+  &:hover {
+    background-color: #007FFF;
+    color: white;
+  }
+  transition:
+    background .3s linear,
+    color .3s linear;
+  border: none;
+  outline: none;
+  appearance: none;
+  cursor: pointer;
 `
 
+const gravitys = [
+  'NorthWest',
+  'North',
+  'NorthEast',
+  'West',
+  'Center',
+  'East',
+  'SouthWest',
+  'South',
+  'SouthEast',
+]
 
 class TemplateConfig extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      gravity: ''
+    }
+
+    this.changeGravity = this.changeGravity.bind(this)
+  }
+
+  changeGravity(gravity) {
+    this.setState({
+      gravity
+    })
   }
 
   render() {
+    const listItem = gravitys.map((gravity, index) => {
+      return (
+        <Item
+          key={ index }
+          active={ this.state.gravity === gravity ? true : false }
+          onClick= { this.changeGravity.bind(this, gravity) }
+          >
+          { gravity }
+        </Item>
+      )
+    })
     return (
       <WrapperItem>
-        <Item>
-          position1
-        </Item>
-        <Item>
-          position2
-        </Item>
-        <Item>
-          position3
-        </Item>
-        <Item>
-          position4
-        </Item>
-        <Item>
-          position5
-        </Item>
-        <Item>
-          position6
-        </Item>
-        <Item>
-          position7
-        </Item>
-        <Item>
-          position8
-        </Item>
-        <Item>
-          position9
-        </Item>
+        { listItem }
       </WrapperItem>
     )
   }
