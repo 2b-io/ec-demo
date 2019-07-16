081d0aa7f9223d73a99faa3a040ef9a73309c8b9
cuongtv <truongcuong90@gmail.com>
2019-07-16 11:53:25 +0700

    Fix bug saga

--- src/resources/app/state/saga/upload.js
+++ src/resources/app/state/saga/upload.js
@@ -1,13 +1,15 @@
-import { take, fork, put, select } from 'redux-saga/effects'
+import { take, fork, put, select, all } from 'redux-saga/effects'
 
 import upload from 'app/services/upload'
 
 import { actions, selectors, types } from 'app/state/interface'
-const getUploadIdentifier = function*() {
+
+const getUploadIdentifierLoop = function*() {
   while (true) {
     try {
-      const a = yield take(types.upload.GET_UPLOAD_IDENTIFIER)
+      yield take(types.upload.GET_UPLOAD_IDENTIFIER)
       const identifier = upload.getRequestId()
+
     } catch (e) {
       console.log('e', e);
     }
@@ -15,6 +17,8 @@ const getUploadIdentifier = function*() {
 }
 
 export default function*() {
-  yield take('@@INITIALIZED')
-  yield fork(getUploadIdentifier)
+  yield all([
+    take('@@INITIALIZED'),
+    fork(getUploadIdentifierLoop)
+  ])
 }
