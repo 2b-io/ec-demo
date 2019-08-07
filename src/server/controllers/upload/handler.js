01af124d96c2c5e1003cbbfe1ef331d27e60df6b
cuongtv <truongcuong90@gmail.com>
2019-08-07 16:45:41 +0700

    Change to new flow

--- src/server/controllers/upload/handler.js
+++ src/server/controllers/upload/handler.js
@@ -1,4 +1,3 @@
-import fsNode from 'fs'
 import fs from 'fs-extra'
 import mime from 'mime-types'
 import path from 'path'
@@ -23,69 +22,15 @@ export default {
     async (req, res, next) => {
       const { requestid: requestId } = req.params
 
-      // check exist file zip and unzip
-      const zipFile = path.resolve(`${ config.uploadimageDir }/${ requestId }/${ requestId }.zip`)
-
-      if (fsNode.existsSync(zipFile)) {
-        console.log('START_UNZIP_FILE...')
-
-        const unzipPath = path.resolve(`${ config.uploadimageDir }/${ requestId }`)
-        const zipPathFile = path.resolve(`${ config.uploadimageDir }/${ requestId }/${ requestId }.zip`)
-
-        await fs.createReadStream(zipPathFile).pipe(unzipper.Extract({ path: unzipPath })).promise()
-        await fs.removeSync(zipPathFile)
-
-        console.log('UNZIP_FILE_DONE...')
-      }
-
-      // upload images to s3
-      console.log('START_UPLOAD_IMAGE_TO_S3...')
-
-      const images = await fsNode.readdirSync(path.resolve(`${ config.uploadimageDir }/${ requestId }`))
-
-      await Promise.all(images.map(async (file, index) => {
-        const imagePath = path.resolve(`${ config.uploadimageDir }/${ requestId }`, file)
-        const contentTypeImage = mime.lookup(imagePath)
-
-        const s3Image = await s3Cache.put(`${ requestId }/images/${ uuid.v4() }`, imagePath, contentTypeImage)
-
-        const { key: s3ImageKey } = s3Image
-
-        const s3Images = cacheRequest.get(requestId).images || []
-        s3Images.push(s3ImageKey)
-
-        cacheRequest.update(requestId, 'images', s3Images)
-      }))
-
-      console.log('UPLOAD_IMAGE_TO_S3_DONE...')
-      // remove folder images
-      await fs.removeSync( path.resolve(`${ config.uploadimageDir }/${ requestId }`))
-
-      // upload  watermark to s3
-      console.log('START_WATERMARK_TO_S3...')
-
-      const watermarkPath = path.resolve(`${ config.uploadWatermarkDir }/${ requestId }/${ requestId }.png`)
-
-      const contentTypeWatermark = mime.lookup(watermarkPath)
-
-      const watermarkS3 = await s3Cache.put(`${ requestId }/watermark/${ uuid.v4() }`, watermarkPath, contentTypeWatermark)
-
-      const { key: watermarkKey } = watermarkS3
-
-      cacheRequest.update(requestId, 'watermark', watermarkKey)
-
-      console.log('WATERMARK_TO_S3_DONE...')
-      // remove folder watermark
-      await fs.removeSync( path.resolve(`${ config.uploadWatermarkDir }/${ requestId }`))
-
       await fs.ensureDir(`${ config.imageResultDir }/${ requestId }`)
 
       const { bucket, s3Key, config: { gravity, padding, opacity } } = await configImage.get(requestId)
+
       const s3KeyOriginImage = cacheRequest.get(requestId).images
 
-      const s3Watermark = cacheRequest.get(requestId).watermark
-
       console.log('GET_WATERMARK_FROM_S3...')
+
+      const s3Watermark = cacheRequest.get(requestId).watermark
 
       const watermarkPathS3 = await s3Cache.get(s3Watermark, 'watermark', requestId)
 
@@ -95,6 +40,8 @@ export default {
         const file = await s3Cache.get(key, 'images',requestId)
         const ext = mime.extension(mime.lookup(file))
         const onputFilePath = await path.resolve(`${ config.imageResultDir }/${ requestId }/${ uuid.v4() }.${ ext }`)
+
+        console.log('PROCESS_IMAGE...', file);
 
         await overlayImage(file, watermarkPathS3, onputFilePath, gravity, requestId, padding, opacity)
       }))
@@ -115,8 +62,7 @@ export default {
 
       // remove folder temp imange result and s3 download
       await fs.removeSync(folderImangeResult)
-      // await fs.removeSync(path.resolve(`${ config.s3DownloadDir }/${ requestId }`))
-      //
+      await fs.removeSync(path.resolve(`${ config.s3DownloadDir }/${ requestId }`))
 
       const linkDownload = `/download/${ requestId }.zip`
 
--- src/server/controllers/upload/image.js
+++ src/server/controllers/upload/image.js
@@ -1,14 +1,17 @@
 import bodyParser from 'body-parser'
 import promise from 'bluebird'
 import formidable from 'formidable'
+import mime from 'mime-types'
 import fs from 'fs-extra'
 import fsNode from 'fs'
 import ms from 'ms'
 import path from 'path'
 import uuid from 'uuid'
+import unzipper from 'unzipper'
 
 import config from 'infrastructure/config'
 import cache from 'services/cache'
+import s3Cache from 'services/cache'
 import cacheRequest from 'services/cache-request'
 import configImage from 'services/config-image'
 
@@ -61,7 +64,20 @@ export default {
           }
         }
 
-        await configImage.create(requestId, '', '', { gravity, padding: { paddingTop, paddingLeft, paddingRight, paddingBottom }, opacity })
+        await configImage.create(
+          requestId,
+          '',
+          '',
+          {
+            gravity,
+            padding: {
+              paddingTop,
+              paddingLeft,
+              paddingRight,
+              paddingBottom
+            },
+          opacity
+        })
 
         const tempPath = files.file.path
         const chunk = parseInt(fields.chunk, 10)
@@ -74,13 +90,70 @@ export default {
           if (err) return next(err)
 
           fs.unlinkSync(tempPath)
-          if (chunk <= chunks - 1) {
+
+          if (chunk < chunks - 1) {
             return res.sendStatus(200)
+          }
+
+          const contentType = mime.lookup(storePath)
+
+          //  check file type upload
+          if (contentType === 'application/zip') {
+            // unzip file image and upload to aws s3
+            console.log('START_UNZIP_FILE...')
+
+            const unzipPath = path.resolve(`${ config.uploadimageDir }/${ requestId }`)
+
+            await fs.createReadStream(storePath).pipe(unzipper.Extract({ path: unzipPath })).promise()
+            console.log('UNZIP_FILE_DONE...')
+
+            console.log('START_UPLOAD_IMAGE_TO_S3...')
+
+            const images = await fsNode.readdirSync(path.resolve(`${ config.uploadimageDir }/${ requestId }`))
+
+            await Promise.all(images.map(async (file, index) => {
+              const imagePath = path.resolve(`${ config.uploadimageDir }/${ requestId }`, file)
+              const contentTypeImage = mime.lookup(imagePath)
+
+              // check file type no upload s3 if file type = zip
+              if (contentTypeImage !=='application/zip') {
+                const s3Image = await s3Cache.put(`${ requestId }/images/${ uuid.v4() }`, imagePath, contentTypeImage)
+
+                const { key: s3ImageKey } = s3Image
+                // put image to cache
+
+                const s3Images = cacheRequest.get(requestId).images || []
+                s3Images.push(s3ImageKey)
+                cacheRequest.update(requestId, 'images', s3Images)
+              }
+            }))
+
+            //  remove file zip after upload to aws s3
+            await fs.remove(storePath)
+
+            res.send('Upload Completed')
+          } else {
+            // upload file image or watermark to s3
+            const s3Image = await s3Cache.put(`${ requestId }/${ filetype }/${ uuid.v4() }`, storePath, contentType)
+
+            const { key: s3ImageKey } = s3Image
+
+            // update s3FileKey to Cache
+            if (filetype === 'image') {
+              // put image to cache
+              const s3Images = cacheRequest.get(requestId).images || []
+              s3Images.push(s3ImageKey)
+              cacheRequest.update(requestId, 'images', s3Images)
+            } else {
+            // put watermark to cache
+              cacheRequest.update(requestId, 'watermark', s3ImageKey)
+            }
+
+            res.send('Upload Completed')
           }
         })
 
         ws.on('error', err => next(err))
-
         rs.pipe(ws)
       })
     },
