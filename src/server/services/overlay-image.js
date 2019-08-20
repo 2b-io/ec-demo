96646e3bc5a38b6391be9220142b45443e9922f5
cuongtv <truongcuong90@gmail.com>
2019-08-20 09:48:04 +0700

    Fix bug resize watermark

--- src/server/services/overlay-image.js
+++ src/server/services/overlay-image.js
@@ -25,32 +25,51 @@ const overlay = async (
   const paddingRight = Number(padding.paddingRight) || 0
   const paddingBottom = Number(padding.paddingBottom) || 0
 
-  if (modeResize && percentTemplate !== 100) {
-    await resizeWatermark(
+  let watermarkPathNew
+  if (modeResize) {
+    watermarkPathNew = await resizeWatermark(
       filePath,
       watermarkPath,
       modeResize,
       heightTemplate,
       widthTemplate,
-      percentTemplate
+      percentTemplate,
+      requestId
     )
   }
 
   if (paddingTop || paddingLeft || paddingRight || paddingBottom) {
     await paddingWatermark(requestId, { paddingTop, paddingLeft, paddingRight, paddingBottom }, watermarkPath)
   }
+  if (watermarkPathNew) {
+    // const {
+    //   width: widthWatermark,
+    //   height: heightWatermark
+    // } = await gm(watermarkPathNew).sizeAsync()
 
-  const {
-    width: widthWatermark,
-    height: heightWatermark
-  } = await gm(watermarkPath).sizeAsync()
+    await gm(filePath)
+      .composite(watermarkPathNew)
+      // .resize(600, 800)
+      .gravity(gravity)
+      .dissolve(opacity)
+      .writeAsync(onputFilePath)
+  } else {
+    // const {
+    //   width: widthWatermark,
+    //   height: heightWatermark
+    // } = await gm(watermarkPath).sizeAsync()
 
-  await gm(filePath)
-    .composite(watermarkPath)
-    // .resize(600, 800)
-    .gravity(gravity)
-    .dissolve(opacity)
-    .writeAsync(onputFilePath)
+    await gm(filePath)
+      .composite(watermarkPath)
+      // .resize(600, 800)
+      .gravity(gravity)
+      .dissolve(opacity)
+      .writeAsync(onputFilePath)
+  }
+
+
+
+
 
 }
 
--- src/server/services/resize-watermark.js
+++ src/server/services/resize-watermark.js
@@ -1,31 +1,46 @@
+import fs from 'fs-extra'
 import gm from 'gm'
+import path from 'path'
 import promise from 'bluebird'
+import mime from 'mime-types'
+import uuid from 'uuid'
+
+import config from 'infrastructure/config'
+
 
 promise.promisifyAll(gm.prototype)
 
-const resizeWatermark = async (imagePath, watermarkPath, modeResize, heightWatermark, widthWatermark, percent) => {
+const resizeWatermark = async (imagePath, watermarkPath, modeResize, heightWatermark, widthWatermark, percent, requestId) => {
   const {
     width: widthOriginImage,
     height: heightOriginImage
   } = await gm(imagePath).sizeAsync()
 
+    const ext = mime.extension(mime.lookup(watermarkPath))
+    await fs.ensureDir(`${ config.s3DownloadDir }/${ requestId }/watermarkResize`)
+    const watermarkPathNew = await path.resolve(`${ config.s3DownloadDir }/${ requestId }/watermarkResize/${ uuid.v4() }.${ ext }`)
+
+    let widthWatermarkNew = widthOriginImage * (percent / 100 )
+
     if (modeResize === 'noKeepRatioPercent') {
-      let widthWatermarkNew = String(widthOriginImage * (percent / 100 )) || null
-      let heightWatermarkNew = String(heightOriginImage * (percent / 100 )) || null
-      await gm(watermarkPath).resize(widthWatermarkNew, heightOriginImage, "!").writeAsync(watermarkPath)
+      let heightWatermarkNew = (heightOriginImage * (percent / 100 )) || null
+      await gm(watermarkPath).resize(widthWatermarkNew,heightWatermarkNew,"!").writeAsync(watermarkPathNew)
+      return watermarkPathNew
     }
 
     if (modeResize === 'percent' || modeResize === 'keepRatioPercent') {
-      let widthWatermarkNew = String(widthOriginImage * (percent / 100 )) || null
-      await gm(watermarkPath).resize(String(widthWatermarkNew),null).writeAsync(watermarkPath)
+      await gm(watermarkPath).resize(widthWatermarkNew,null).writeAsync(watermarkPathNew)
+      return watermarkPathNew
     }
 
     if (modeResize === 'pixel'|| modeResize === 'keepPercentPixel') {
-      await gm(watermarkPath).resize(widthWatermark, heightWatermark).writeAsync(watermarkPath)
+      await gm(watermarkPath).resize(widthWatermark,heightWatermark).writeAsync(watermarkPathNew)
+      return watermarkPathNew
     }
 
     if (modeResize === 'noKeepPercentPixel') {
-      await gm(watermarkPath).resize(widthWatermark, heightWatermark, "!").writeAsync(watermarkPath)
+      await gm(watermarkPath).resize(widthWatermark,heightWatermark,"!").writeAsync(watermarkPathNew)
+      return watermarkPathNew
     }
 }
 
